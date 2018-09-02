a1b2fef7a9786535af3f9efe1a79a17ec7ac7567
faisal almutairi <42886225+FAISALhhh@users.noreply.github.com>
2018-09-02 03:50:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
+const prefix = "%";
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+    console.log('I am ready!');
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong!');
+      }
+});
+
+   client.on('ready', () => {
+     client.user.setActivity("NoThing",{type: 'WATCHING'});
+ });
+
+
+  client.login(process.env.BOT_TOKEN);
